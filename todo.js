73fe51dd60f4da779565f538c425b463c0942146
mohamad <mohamad.epic.k@gmail.com>
2024-07-09 15:07:18 +0330

    fixed localstorage

--- todo.js
+++ todo.js
@@ -1,48 +1,53 @@
-import "bootstrap"
-import * as todoData from "./data/todo.json"
 
-const todos = todoData.default;
+let mainData
+async function  getData(){
+    mainData = await fetch("./data/todo.json")
 
-
-const getdata = async () => {
-    const data = await JSON.parse(localStorage.getItem('todos')) || [];
-    return data;
+    mainData = await mainData.json()
 
 }
-const todoTask =  (todo) => {
 
+const todoTask =  (todo) => {
     const content = document.querySelector('.container')
     let view = '';
+
     todo.forEach(tod => {
-
-        view += `<div id="${tod.id}">
-            <input id="task${tod.id}" type="checkbox" onchange="clickHandeler()"/>
-            <label htmlFor="task${tod.id}">${tod.title} </label>
-           
+        view += `<div id="${tod.id}" >
+            <input id="task${tod.id}" type="checkbox" onchange="hand(${tod.id},${tod.completed})" ${tod.completed == true ? 'checked' : ''} />
+            <label For="task${tod.id}">${tod.title} </label>
         </div>`
-
-
     })
-
     content.innerHTML = view;
 }
 
 window.addEventListener('load', async () => {
-    const data1 = await getdata()
-    console.log(data1)
-    todoTask(data1)
 
+    await getData()
+    if(mainData.length == 0){
+        localStorage.setItem('data' , JSON.stringify(mainData))
+    }
+
+    const localData =  JSON.parse(localStorage.getItem('data'))
+
+    console.log(localData)
+
+    todoTask(localData)
 
 })
-const clickHandeler = () => {
-    console.log('dsf')
-    // data.forEach(tod=>{
-    //     tod.id !== id ;
-    //     localStorage.setItem('todos', tod[id].completed)
-    //
-    // })
-    // console.log(${tod.completed})
-}
 
+function hand(id , completed ){
+    const local = JSON.parse(localStorage.getItem('data'))
+    local.map(loc=>{
+        if (loc.id === id){
+            loc.completed = !completed;
+            console.log(loc.completed)
+
+
+        }
+
+    })
+    localStorage.setItem('data' , JSON.stringify(local))
+
+}
 
 
